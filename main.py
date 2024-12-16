36461e2165a8ce9a6e37618a8eec3c1606b9eb18
JitDun <cal.dun08@gmail.com>
2024-12-16 13:35:14 +1100

    Player movement

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
-        dt = clock.tick()/1000
+        dt = clock.tick(60)/1000
 
 
 
